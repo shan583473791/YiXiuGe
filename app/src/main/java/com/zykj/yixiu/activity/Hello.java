65823eefde5507126ed13397dbccad2f5f67f842
shan583473791 <583473790@qq.com>
2017-04-08 13:45:09 +0800

    添加了11个文件包 并且测试了一个Hell

---
+++ app/src/main/java/com/zykj/yixiu/activity/Hello.java
@@ -0,0 +1,16 @@
+package com.zykj.yixiu.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zykj on 2017/4/8.
+ */
+
+public class Hello extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
