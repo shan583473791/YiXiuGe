428cba9d54475e12a00c56ef43c11a07fc808570
zykj <583473790@qq.com>
2017-04-11 15:45:27 +0800

    添加了手机维修Activity。完事

--- app/src/main/java/com/zykj/yixiu/activity/Hello.java
+++ app/src/main/java/com/zykj/yixiu/activity/Hello.java
@@ -14,7 +14,7 @@ public class Hello extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_include_title);
+        setContentView(R.layout.activity_main);
         MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
     }
 }
